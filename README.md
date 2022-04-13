03a3e9cbaf12b0fc225809d88f818cc8cca5fc20
Rasse <61808400+RasseTheBoy@users.noreply.github.com>
2022-04-13 03:55:27 +0300

    Project files added

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# Infinite-Monkey-Theorem
-Monkey ⌨️🐒
+
+# Infinite Monkey Theorem
+
+We all love monkeys 🐒  
+And so do mathematicians and theory lovers as well.
+
+With this script, you give your desired word, and wait for the monkeys to write your sentence.
+
+
+## Made by
+
+- [@RasseTheBoy](https://github.com/RasseTheBoy)
+## TL;DR About the theory
+
+The infinite monkey theorem states that a monkey hitting keys at random on a  
+typewriter keyboard for an infinite amount of time will almost surely type any  
+given text, such as the complete works of William Shakespeare.
+
+(source: [Wikipedia](https://en.wikipedia.org/wiki/Infinite_monkey_theorem))
+## Required libraries
+
+(Only standard libraries)
+
+- threading
+- datetime
+- string
+- random
+- time
+## Tutorial
+
+1. Run the **main.py** file
+
+```
+python main.py
+```
+
+```
+python3 main.py
+```
+
+2. Enter the amount of monkeys (only if **monkey_names.txt** is empty)
+
+3. Give the sentence you want the monkeys to generate
+
+4. Wait...
+
+
+## Demo
+
+![demo video](https://media.giphy.com/media/05G5vtY24iqSV6hTer/giphy.gif)
+## monkey_names.txt
+
+You can give each monkey its own unique name.  
+Just write them as shown below:  
+
+```
+Mike
+Alfonso
+Herbert
+Yoda
+```
+If this file is empty, the code asks how many monkeys to create.  
+(The default name for a monkey is "Mike")
+
+## Output
+
+| Output        | Description |
+| ------------- |:-------------:|
+| Name | Name of the monkey |
+| Generated sentence | The sentence, the monkey has generated |
+| Generated amount | Amount of sentences the monkey has generated |
+| Elapsed time | How long it took for the monkey to generate the sentence |
---
+++ main.py
@@ -0,0 +1,90 @@
+import threading
+from datetime   import timedelta
+from string     import ascii_lowercase
+from random     import choice
+from time   import time
+
+# ------------------------------------------------------------
+
+class Monkey:
+    def __init__(self, name='Mike'):
+        self.ascii_lowercase = [x for x in ascii_lowercase + ' ']
+        self.name = name
+
+    def find_sentence(self, sentence):
+        self.start_time = time()
+        self.sentence = sentence.lower()
+        self.sentence_len = len(sentence)
+        self.generated_sentence_amnt = 0
+
+        while True:
+            self.generated_sentence_amnt += 1
+            self.generated_sentence = ''
+            for indx in range(self.sentence_len):
+                generated_char = choice(self.ascii_lowercase)
+                if generated_char == self.sentence[indx]:
+                    self.generated_sentence += generated_char
+                else:
+                    break
+
+            if self.generated_sentence == self.sentence:
+                self.end_time = time()
+                self.elapsed_time = timedelta(seconds=self.end_time-self.start_time)
+                break
+
+        fprint(
+f'''Name: {self.name}
+Generated sentence: {self.generated_sentence}
+Generated amount: {add_space_for_int(self.generated_sentence_amnt)}
+Elapsed time: {self.elapsed_time}
+'''
+        )
+
+# ------------------------------------------------------------
+
+def fprint(text):
+    print(f'{text}\n')
+
+def get_input(text, typ=str):
+    while True:
+        inpt = input(text); print()
+        try:
+            return typ(inpt)
+        except:
+            print('Invalid input!')
+            fprint('Input needs to be: ' + str(typ).split('\'')[1])
+
+def add_space_for_int(i):
+    i = ''.join([x+' ' if (indx+1)%3 == 0 else x for indx, x in enumerate(str(i)[::-1])])[::-1]
+    if i[0] == ' ':
+        i = ''.join(list(i)[1:])
+    return i
+
+# ------------------------------------------------------------
+
+def get_monkey_names():
+    with open('monkey_names.txt', 'r') as f:
+        lines = list(filter(None, (line.rstrip() for line in f)))
+    return lines
+
+# ------------------------------------------------------------
+
+def main():
+    monkey_nam_lst = get_monkey_names()
+    if not monkey_nam_lst:
+        monkey_nam_lst = [f'Mike {x}' for x in range(get_input('Amount of monkeys: ', int))]
+
+    monkey_lst = [Monkey(monkey_nam_lst[x]) for x in range(len(monkey_nam_lst))]
+
+    fprint('--- Don\'t use numbers in the sentence ---')
+    sentence = get_input('Sentence to find: ', str)
+    print("Fun fact: The posibility to find your unique string is approximately 1/" +  add_space_for_int(27**len(sentence)) + '\n')
+
+    thread_lst = [ threading.Thread(target=monkey.find_sentence, args=(sentence,)) for monkey in monkey_lst]
+    for th in thread_lst:
+        th.start()
+
+# ------------------------------------------------------------
+
+if __name__ == '__main__':
+    main()
